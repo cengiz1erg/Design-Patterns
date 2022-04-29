2718484f403564e5031389fbc5db9d1c65de52b4
Cengiz Ergün <cengiz1ergun@gmail.com>
2022-04-30 00:01:14 +0300

    Singleton

---
+++ SingletonA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton2 = Singleton.GetInstance();
+            Singleton singleton1 = Singleton.GetInstance();
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("Esittir.");
+            }
+        }
+    }
+}
---
+++ SingletonA/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonA
+{
+    class Singleton
+    {
+        private static Singleton _instance;
+        private Singleton()
+        {
+
+        }
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
